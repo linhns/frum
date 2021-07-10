c86252a08aea5d0bb067067d1ec8e1c224e3a49d
Nguyen Son Linh <linhns59@gmail.com>
2021-07-10 15:56:12 +0800

    Add Ransack

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true)
   end
 
   # GET /posts/1 or /posts/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :replies
   end
   get 'profile', to: 'profile#index'
+  # get 'search', to: 'posts#search'
   devise_for :users, :skip => [:registrations]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
